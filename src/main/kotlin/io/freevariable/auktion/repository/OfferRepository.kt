bddd606b665e17fe7f0dc279e2852c1604ba1071
a-fatah <abdulfatah66@gmail.com>
2023-02-28 08:32:37 +0100

    add search resource

--- src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
+++ src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
@@ -3,8 +3,10 @@ package io.freevariable.auktion.repository
 import io.freevariable.auktion.model.Offer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 
 @RepositoryRestResource
 interface OfferRepository : JpaRepository<Offer, Long> {
-    fun findByOpen(open: Boolean): List<Offer>
+    @RestResource(path = "by-status", rel = "by-status")
+    fun findByOpen(open: Boolean = true): List<Offer>
 }
