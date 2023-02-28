0c4685d9ab41b81dd881f2000862af3cdd6df46a
a-fatah <abdulfatah66@gmail.com>
2023-02-28 06:25:32 +0100

    expose Offer repository via spring data rest

--- src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
+++ src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
@@ -2,7 +2,9 @@ package io.freevariable.auktion.repository
 
 import io.freevariable.auktion.model.Offer
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource
 interface OfferRepository : JpaRepository<Offer, Long> {
     fun findByOpen(open: Boolean): List<Offer>
 }
