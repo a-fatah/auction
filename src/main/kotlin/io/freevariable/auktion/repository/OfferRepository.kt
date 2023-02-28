0cd65c9c25908515676288758bdf3e4f057bee93
a-fatah <abdulfatah66@gmail.com>
2023-02-28 09:02:27 +0100

    use OfferView as default projection

--- src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
+++ src/main/kotlin/io/freevariable/auktion/repository/OfferRepository.kt
@@ -1,11 +1,12 @@
 package io.freevariable.auktion.repository
 
+import io.freevariable.auktion.controller.OfferView
 import io.freevariable.auktion.model.Offer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.rest.core.annotation.RestResource
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = OfferView::class)
 interface OfferRepository : JpaRepository<Offer, Long> {
     @RestResource(path = "by-status", rel = "by-status")
     fun findByOpen(open: Boolean = true): List<Offer>
